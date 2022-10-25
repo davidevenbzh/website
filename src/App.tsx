b2e225512ce0b6f8358c11f04a2b075e2cb2fa54
davidevenbzh <pro.davideven@gmail.com>
2022-10-25 17:35:37 +0200

    build(tailwind): add tailwind in project)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
+import { version } from 'pkginfo';
 
 function App(): JSX.Element {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto px-4'>
+      <h1 className='text-3xl font-bold text-center'>David Even</h1>
+      <footer>{version}</footer>
     </div>
   );
 }
