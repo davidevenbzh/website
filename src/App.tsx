0621578486484a0e745c1d8e130f6fdc6c9527b6
davidevenbzh <pro.davideven@gmail.com>
2022-10-27 10:27:19 +0200

    fix(app): remove version number will find better solution latter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import { version } from 'pkginfo';
 
 function App(): JSX.Element {
   return (
     <div className='container mx-auto px-4'>
       <h1 className='text-3xl font-bold text-center'>David Even</h1>
-      <footer>{version}</footer>
     </div>
   );
 }
